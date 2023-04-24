b10d9159a01d891afeee513c8a73789d157c5fb6
MariaKnitareva <enuw13@gmail.com>
2023-04-24 20:01:18 +0600

    Реализован метод REST-API pet1-pet3

--- src/main/java/com/billiard/website/controllers/MainController.java
+++ src/main/java/com/billiard/website/controllers/MainController.java
@@ -2,7 +2,6 @@ package com.billiard.website.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
